ea6d4e11044083244984638cb17d9237f0920bc8
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-02-19 10:10:26 -0600

    created models for address, customer & employee

---
+++ TrashCollector/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Address
+    {
+    }
+}
